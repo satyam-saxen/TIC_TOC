798acc3d44199287bd5ae0882eeab75263f729ab
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-15 13:07:52 +0530

    Add text on click

--- functionality.js
+++ functionality.js
@@ -1,8 +1,18 @@
+let flag = 1;
 let numberOfButtons = document.querySelectorAll(".grid-item").length;
 for(let index=0;index<numberOfButtons;index++){
 	document.querySelectorAll(".grid-item")[index].addEventListener("click",handleClick);
 }
 function handleClick(){
 	let box = this.id;
-	console.log(box);
+	let checkForEmpty = this.innerHTML;
+	if(checkForEmpty == ""){
+		if(flag == 1){
+			this.innerHTML = "X";
+			flag = 0;
+		}else{
+			this.innerHTML = "O";
+			flag = 1;
+		}
+	} 
 }
