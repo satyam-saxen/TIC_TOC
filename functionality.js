81abeca4d97b4dcad7f3d52f007a4952d00a9205
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-15 12:54:06 +0530

    Add function for handle click

---
+++ functionality.js
@@ -0,0 +1,8 @@
+let numberOfButtons = document.querySelectorAll(".grid-item").length;
+for(let index=0;index<numberOfButtons;index++){
+	document.querySelectorAll(".grid-item")[index].addEventListener("click",handleClick);
+}
+function handleClick(){
+	let box = this.innerHTML;
+	console.log(box);
+}
